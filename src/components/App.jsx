ec48eed2b94957a0ef94bbd27e2302a08820febc
Mlodzieniak <116485218+Mlodzieniak@users.noreply.github.com>
2023-05-11 12:13:30 +0200

    login, signup and / are now public routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,55 @@
 import React from "react";
 import "../App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import PrivateRoute from "./PrivateRoute";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+// import PrivateRoute from "./PrivateRoute";
 import Signup from "./Signup";
 import Dashboard from "./Dashboard";
 import Login from "./Login";
 import Home from "./Home";
+import { useAuth } from "../contexts/AuthContext";
+
+function PrivateRoute({ children }) {
+  const { currentUser } = useAuth();
+  return currentUser ? children : <Navigate to="/login" />;
+}
+function PublicRoute({ children }) {
+  const { currentUser } = useAuth();
+  return !currentUser ? children : <Navigate to="/home" />;
+}
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
+        <Route
+          path="/"
+          element={
+            <PublicRoute>
+              <Dashboard />
+            </PublicRoute>
+          }
+        />
+        <Route
+          path="/signup"
+          element={
+            <PublicRoute>
+              <Signup />
+            </PublicRoute>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <PublicRoute>
+              <Login />
+            </PublicRoute>
+          }
+        />
         <Route
           path="/home"
           element={
@@ -22,8 +58,6 @@ function App() {
             </PrivateRoute>
           }
         />
-        {/* <PrivateRoute path="/home" element={<Home />} /> */}
-        {/* <Route path="/home" element={<PrivateRoute component={Home} />} /> */}
       </Routes>
     </Router>
   );
