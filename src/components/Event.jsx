d64a590a99cd2f1cd017696e09b94fa9e6a4e533
Mlodzieniak <116485218+Mlodzieniak@users.noreply.github.com>
2023-05-12 10:33:09 +0200

    add timestamp formating to event

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -3,11 +3,16 @@ import { Avatar } from "@mui/material";
 
 export default function Event({ data }) {
   const { authorDisplayName, authorAvatarURL, postedAt, text, imageURL } = data;
+  const date = postedAt.toDate(); // Convert timestamp to Date object
+  const formattedDate = date.toLocaleDateString("en-GB"); // Format date as dd/mm/yyyy
+  const formattedTime = date.toLocaleTimeString("en-US", { hour12: false }); // Format time as 24-hour format
+  const formattedTimestamp = `${formattedDate} ${formattedTime}`;
+
   return (
     <div>
       <Avatar src={authorAvatarURL} />
       <div className="author">{authorDisplayName}</div>
-      <div className="post-time">{postedAt.seconds}</div>
+      <div className="post-time">{formattedTimestamp}</div>
       <div className="post-text">{text}</div>
       {imageURL ? <img src={imageURL} alt="#" /> : null}
     </div>
